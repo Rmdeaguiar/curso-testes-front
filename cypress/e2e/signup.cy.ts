0ca293fa90f8df464c42e32a5b8b7b3b7db1e801
Rafael <rafael.aguiar91@gmail.com>
2024-04-10 18:04:18 -0300

    test: criação dos testes e2e para página de PokemonDetail

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,6 +1,6 @@
 describe('Tests the SignUp page', () => {
 
-    it('should go to Login page when clicks on Cadastro', () => {
+    it('should go to Login page when clicks on "Já tem cadastro?"', () => {
       cy.visit('/sign-up');
 
       cy.contains('Já tem cadastro? Clique aqui!').click();
